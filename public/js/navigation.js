912d6e0609bad469fd050fe01b872976fd195f06
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-10-22 15:09:44 -0600

    pilot-project-ready with navigation implement with javascript

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,42 +1,58 @@
+function handleClickPrincipal(event) {
+    event.preventDefault();
+    templateMain('principalPage');
+}
+function handleClickAbout(event) {
+    event.preventDefault();
+    templateMain('aboutPage');
+}
+function handleClickProject(event) {
+    event.preventDefault();
+    templateMain('projectsPage');
+}
+function handleClickSkills(event) {
+    event.preventDefault();
+    templateMain('skillsPage');
+}
+function handleClickCertific(event) {
+    event.preventDefault();
+    templateMain('certificationsPage');
+}
 
-function addMainListeners() {
-    const principalDirection = document.querySelectorAll('.principalDir');
-    principalDirection.forEach(principal => {
-        principal.addEventListener('click', (event) => {
-            event.preventDefault();
-            console.log('Presionaste el boton principal');
-        })    
-    });
+async function addListeners() {
 
-    const aboutDirection = document.querySelectorAll('.aboutDir');
-    aboutDirection.forEach(about => {
-        about.addEventListener('click', (event) => {
-            event.preventDefault();
-            console.log('Presionaste el boton about');
-        })    
-    });
+    try {
+        const principalDirection = document.querySelectorAll('.principalDir');
+        principalDirection.forEach(principal => { 
+            principal.removeEventListener('click', handleClickPrincipal);
+            principal.addEventListener('click', handleClickPrincipal);
+        });
     
-    const projectsDirection = document.querySelectorAll('.projectDir');
-    projectsDirection.forEach(project => {
-        project.addEventListener('click', (event) => {
-            event.preventDefault();
-            console.log('Presionaste el boton project');
-        })    
-    });
-    
-    const skillsDirection = document.querySelectorAll('.skillsDir');
-    skillsDirection.forEach(skill => {
-        skill.addEventListener('click', (event) => {
-            event.preventDefault();
-            console.log('Presionaste el boton skill');
-        })    
-    });
-    
-    const certificDirection = document.querySelectorAll('.certificDir');
-    certificDirection.forEach(certific => {
-        certific.addEventListener('click', (event) => {
-            event.preventDefault();
-            console.log('Presionaste el boton certification');
-        })    
-    });
+        const aboutDirection = document.querySelectorAll('.aboutDir');
+        aboutDirection.forEach(about => {  
+            about.removeEventListener('click', handleClickAbout);
+            about.addEventListener('click', handleClickAbout);
+        });
+        
+        const projectsDirection = document.querySelectorAll('.projectDir');
+        projectsDirection.forEach(project => {   
+            project.removeEventListener('click', handleClickProject);
+            project.addEventListener('click', handleClickProject);
+        });
+        
+        const skillsDirection = document.querySelectorAll('.skillsDir');
+        skillsDirection.forEach(skill => {   
+            skill.removeEventListener('click', handleClickSkills);
+            skill.addEventListener('click', handleClickSkills);  
+        });
+        
+        const certificDirection = document.querySelectorAll('.certificDir');
+        certificDirection.forEach(certific => {   
+            certific.removeEventListener('click', handleClickCertific);
+            certific.addEventListener('click', handleClickCertific);
+        });
+        
+    } catch (error) {
+        console.log('No se agregaron los eventos', error);
+    }
 }
--- public/js/templates.js
+++ public/js/templates.js
@@ -1,19 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    loadPage();
-});
+document.addEventListener('DOMContentLoaded', loadPage());
 
 async function loadPage() {
     try {
         await templateHeader();
-        await loadMain();
         await templateFooter();
-        addMainListeners();
+        await templateMain();
 
     } catch (error) {
         console.log('Hubo un error al cargar la pagina', error);
     }
 }
-
 
 async function templateHeader () {
     try {
@@ -21,7 +17,9 @@ async function templateHeader () {
         const response = await fetch('./src/components/templates/header.html');
         const data = await response.text();
         headerContainer.innerHTML = data;
+
         console.log("Header cargado exitosamente");
+        
     } catch (error) {
         console.log('No se cargo el contenido del header', error);
     }
@@ -33,8 +31,43 @@ async function templateFooter () {
         const response = await fetch('./src/components/templates/footer.html');
         const data = await response.text();
         footerContainer.innerHTML = data;
+
         console.log("Footer cargado exitosamente");    
     } catch (error) {
         console.log('No se cargo el contenido del footer', error);
+    }
+}
+
+async function templateMain (page = 'principalPage') {
+    try {
+        let pagePath = '';
+
+        switch(page) {
+            case 'principalPage':
+                pagePath = './src/components/pages/principal/principal.html';
+                break;
+            case 'aboutPage':
+                pagePath = './src/components/pages/about/about.html';
+                break;
+            case 'projectsPage':
+                pagePath = './src/components/pages/projects/project.html';
+                break;
+            case 'skillsPage':
+                pagePath = './src/components/pages/skills/skill.html';
+                break;
+            case 'certificationsPage':
+                pagePath = './src/components/pages/certifications/certification.html';
+                break;
+        }
+
+        const mainContainer = document.getElementById('mainIndex');
+        const response = await fetch(pagePath);
+        const data = await response.text();
+        mainContainer.innerHTML = data;
+        console.log('Main cargado exitosamente');
+        await addListeners();
+
+    } catch (error) {
+        console.log('No se cargo el contenido del main', error);
     }
 }
